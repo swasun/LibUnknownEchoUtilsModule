f38b1ae6cb96106c72bf4d24ab46ee947eed6002
Charly Lamothe <charly.lamothe@sfr.fr>
2018-07-10 15:50:31 +0200

    Fix static lib name

--- external/libei.cmake
+++ external/libei.cmake
@@ -19,7 +19,7 @@ else (systemlib_LIBEI)
 	if (WIN32)
 		set(LIBERRORINTERCEPTOR_LIBRARIES "${ROOT_BUILD_DIR}\\ei_static.lib")
 	else()
-		set(LIBERRORINTERCEPTOR_LIBRARIES ${ROOT_BUILD_DIR}/libei/install/lib/libei.a)
+		set(LIBERRORINTERCEPTOR_LIBRARIES ${ROOT_BUILD_DIR}/libei/install/lib/libei_static.a)
 	endif()
 
 	ExternalProject_Add(libei
