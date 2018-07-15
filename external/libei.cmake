88fdb901cee92f44438b406a66ba8b7440b02a18
Charly Lamothe <charly.lamothe@sfr.fr>
2018-07-15 21:39:44 +0200

    Add licence in libei.cmake and starts thread_example.c

--- external/libei.cmake
+++ external/libei.cmake
@@ -1,3 +1,22 @@
+ ##########################################################################################
+ # Copyright (C) 2018 by Charly Lamothe													  #
+ #																						  #
+ # This file is part of LibErrorInterceptor.										  	  #
+ #																						  #
+ #   LibErrorInterceptor is free software: you can redistribute it and/or modify   		  #
+ #   it under the terms of the GNU General Public License as published by				  #
+ #   the Free Software Foundation, either version 3 of the License, or					  #
+ #   (at your option) any later version.												  #
+ #																						  #
+ #   LibErrorInterceptor is distributed in the hope that it will be useful,        		  #
+ #   but WITHOUT ANY WARRANTY; without even the implied warranty of						  #
+ #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the						  #
+ #   GNU General Public License for more details.										  #
+ #																						  #
+ #   You should have received a copy of the GNU General Public License					  #
+ #   along with LibErrorInterceptor.  If not, see <http://www.gnu.org/licenses/>.  		  #
+ ##########################################################################################
+
 if (LIBEI_SYSTEM)
     if (WIN32)
         set(LIBERRORINTERCEPTOR_INCLUDE_DIR "C:\\LibErrorInterceptor\\$ENV{name}\\include")
