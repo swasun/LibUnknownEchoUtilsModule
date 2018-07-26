b5458e931f9d0ece165d670f1f864038f6d5dddd
Charly Lamothe <charly.lamothe@sfr.fr>
2018-07-26 10:06:45 +0200

    Update licence and copyright from gpl3 to apache2

--- src/ueum/compiler/overflow.h
+++ src/ueum/compiler/overflow.h
@@ -1,30 +1,20 @@
-/******************************************************************************************
- * Copyright (C) 2018 by Charly Lamothe                                                   *
- *                                                                                        *
- * This file is part of LibUnknownEchoUtilsModule.                                        *
- *                                                                                        *
- *   LibUnknownEchoUtilsModule is free software: you can redistribute it and/or modify    *
- *   it under the terms of the GNU General Public License as published by                 *
- *   the Free Software Foundation, either version 3 of the License, or                    *
- *   (at your option) any later version.                                                  *
- *                                                                                        *
- *   LibUnknownEchoUtilsModule is distributed in the hope that it will be useful,         *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of                       *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                        *
- *   GNU General Public License for more details.                                         *
- *                                                                                        *
- *   You should have received a copy of the GNU General Public License                    *
- *   along with LibUnknownEchoUtilsModule.  If not, see <http://www.gnu.org/licenses/>.   *
- ******************************************************************************************/
-
-/*
- * This code is inspired from integer.h of libgit2.
- * 
- * Copyright (C) the libgit2 contributors. All rights reserved.
- *
- * This file is part of libgit2, distributed under the GNU GPL v2 with
- * a Linking Exception. For full terms see the included COPYING file.
- */
+/*******************************************************************************
+ * Copyright (C) 2018 Charly Lamothe                                           *
+ *                                                                             *
+ * This file is part of LibUnknownEchoUtilsModule.                             *
+ *                                                                             *
+ *   Licensed under the Apache License, Version 2.0 (the "License");           *
+ *   you may not use this file except in compliance with the License.          *
+ *   You may obtain a copy of the License at                                   *
+ *                                                                             *
+ *   http://www.apache.org/licenses/LICENSE-2.0                                *
+ *                                                                             *
+ *   Unless required by applicable law or agreed to in writing, software       *
+ *   distributed under the License is distributed on an "AS IS" BASIS,         *
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
+ *   See the License for the specific language governing permissions and       *
+ *   limitations under the License.                                            *
+ *******************************************************************************/
 
 #ifndef UNKNOWNECHOUTILSMODULE_OVERFLOW_H
 #define UNKNOWNECHOUTILSMODULE_OVERFLOW_H
